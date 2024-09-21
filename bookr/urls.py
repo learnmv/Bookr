71d5232e5c892e330bea266ada5238e3904877cd
Mahesh Varma <maheshvarma.dommaraju@gmail.com>
2024-09-20 20:56:06 -0400

    added static files both globally and for reviews app

--- bookr/urls.py
+++ bookr/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for bookr project.
+"""bookr URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 import reviews.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', reviews.views.index),
-    # path('book-search', reviews.views.search),
-    path('', include('reviews.urls'))
-
+    path("admin/", admin.site.urls),
+    path("", reviews.views.index),
+    path("book-search/", reviews.views.book_search, name="book_search"),
+    path("", include("reviews.urls")),
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index" ),
-    path('books/', views.book_list, name='book_list'),
-    path('books/<int:pk>/',views.book_detail, name='book_detail'),
-    path('book/<int:pk>/',views.book_detail, name='detail_bbo')
+    path("books/", views.book_list, name="book_list"),
+    path("books/<int:pk>/", views.book_detail, name="book_detail"),
 ]
